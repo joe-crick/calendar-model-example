5f035d5e464c319e1c5d49ce9773564193900d52
Joe Crick <joe@josephcrick.com>
2017-03-18 08:56:59 +0100

    Basic calendar formatting

--- src/calendar.jsx
+++ src/calendar.jsx
@@ -3,6 +3,7 @@ import cal from 'calendar-model/dist/bundle.js';
 import './calendar.css';
 
 const monthNameFinder = cal.month.monthNameFinder();
+const weekDayNameFinder = cal.week.weekDayNameFinder();
 
 function getAdjustedMonth(month) {
     return parseInt(month, 10) - 1;
@@ -16,12 +17,25 @@ function getDayClassName(day, month) {
     return `day ${(day.date.getMonth() === getAdjustedMonth(month)) ? 'current' : 'outside'}`
 }
 
+function getWeekDayNames() {
+    const names = [];
+    for(let x = 0; x< 7; x++) {
+        names.push(<th>{weekDayNameFinder(x)}</th>)
+    }
+    return names;
+}
+
 const Calendar = props => {
 
     return (
         <div className="calendar-container">
             <h1>{getCalendarTitle(props)}</h1>
             <table className="calendar-example">
+                <thead>
+                    <tr>
+                        {getWeekDayNames()}
+                    </tr>
+                </thead>
                 <tbody>
                     {props.calendarDays.map((week, index) => {
                         return (
@@ -29,16 +43,18 @@ const Calendar = props => {
                                 {week.map((day, idx) => {
                                     return (
                                     <td key={idx} className={getDayClassName(day, props.month)}>
-                                        <h3>{day.dayOfMonth}</h3>
-                                        <ul className="events">
-                                        {day.events.map((ev, idx) => {
-                                            return (
-                                                <li key={idx}>
-                                                    {ev.time}: {ev.title}
-                                                </li>
-                                            )
-                                        })}
-                                        </ul>
+                                        <div className="day-contents">
+                                            <h3>{day.dayOfMonth}</h3>
+                                            <ul className="events">
+                                            {day.events.map((ev, idx) => {
+                                                return (
+                                                    <li key={idx}>
+                                                        {ev.time}: {ev.title}
+                                                    </li>
+                                                )
+                                            })}
+                                            </ul>
+                                        </div >
                                     </td>);
                                 })}                                             
                         </tr>);
